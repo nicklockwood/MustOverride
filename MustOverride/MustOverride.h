292dec873d9cebf57469978feb7dd64cb5c87bab
Nick Lockwood <nicklockwood@fb.com>
2015-02-22 14:34:15 +0000

    Fixed another typo.

--- MustOverride/MustOverride.h
+++ MustOverride/MustOverride.h
@@ -35,7 +35,7 @@
 /**
  * Include this macro inside any class or instance method that MUST be overridden
  * by its subclass(es). The app will then crash immediately on launch with an
- * assertion if the method is not overriden (even if it is never called).
+ * assertion if the method is not overridden (even if it is never called).
  */
 #define SUBCLASS_MUST_OVERRIDE __attribute__((used, section("__DATA,MustOverride" \
 ))) static const char *__must_override_entry__ = __func__
